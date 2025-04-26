c3bdf03c90aafec5ee3f0a4eb81c58cb786cfd8c
Roman Levkiv <romanwdwd@gmail.com>
2025-04-26 14:17:19 +0300

    init server

---
+++ server/ukr-damage-report/src/test/java/com/example/ukrdamagereport/UkrDamageReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ukrdamagereport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkrDamageReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
